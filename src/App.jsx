0a6ef50d46f4d23b25369def476c5b20996445bf
Muhammad Zawwar Akram <156500678+IbtahajAkram@users.noreply.github.com>
2025-01-21 20:55:39 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import CurrencyConverter from "./CurrencyConverter/CurrencyConverter";
+import React from "react";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import CurrencyConverter from "./CurrencyConverter/CurrencyConverter";
 
 const App = () => {
+  
+  return (
 
     <>
     <ToastContainer/>
-   <CurrencyConverter/>
+    <CurrencyConverter/>
     </>
   );
 };
